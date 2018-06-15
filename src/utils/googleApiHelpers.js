dd1df4333ff417afc352407c24546668d9ec03c0
Cal <cal@Admins-MacBook-Air.local>
2018-06-15 15:22:46 -0400

    added markers

--- src/utils/googleApiHelpers.js
+++ src/utils/googleApiHelpers.js
@@ -1,6 +1,7 @@
 export function searchNearby(google, map, request) {
   return new Promise((resolve, reject) => {
     const service = new google.maps.places.PlacesService(map);
+    const request = {placeId}
 
     service.nearbySearch(request, (results, status, pagination) => {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
