9a215c32bf8f65b66f2b6b7f4326cc742ac39411
Cal <cal@Admins-MacBook-Air.local>
2018-06-16 12:37:34 -0400

    made map page the index

--- src/views/main/routes.js
+++ src/views/main/routes.js
@@ -11,7 +11,7 @@ export const makeMainRoutes = () => {
       <Route path="map" component={Map}>
       <Route path="detail/:placeId"
             component={Detail} />
-      </Route>
+      </Route>asdf
       <IndexRoute component={Map} />
     </Route>
   )
